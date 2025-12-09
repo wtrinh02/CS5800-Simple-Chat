9be0700584e3bf2d2c47dc208f9a4aec2d3f996f
Christopher Avakian <chrisavakian@berkeley.edu>
2025-12-09 11:55:32 -0800

    Refactored by adding State design pattern

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -9,68 +9,51 @@ public class User {
     private final Map<String, List<Message>> directMessages;
     private final Set<String> blockedUserIds;
     private boolean online;
+    private UserState state;
 
     public User(
-        String userId,
-        String username,
-        String email,
-        Set<String> friendIds,
-        Map<String, List<Message>> directMessages,
-        boolean online
+            String userId,
+            String username,
+            String email,
+            Set<String> friendIds,
+            Map<String, List<Message>> directMessages,
+            boolean online
     ) {
         this.userId = userId;
         this.username = username;
         this.email = email;
-        this.friendIds = friendIds != null
-            ? new HashSet<>(friendIds)
-            : new HashSet<>();
-        this.directMessages = directMessages != null
-            ? new HashMap<>(directMessages)
-            : new HashMap<>();
-        this.online = online;
+        this.friendIds = friendIds != null ? new HashSet<>(friendIds) : new HashSet<>();
+        this.directMessages = directMessages != null ? new HashMap<>(directMessages) : new HashMap<>();
         this.blockedUserIds = new HashSet<>();
+        this.online = online;
+        this.state = new OfflineState();
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+    public String getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
 
     public Set<String> getFriendIds() {
         return new HashSet<>(friendIds);
     }
 
     public boolean isOnline() {
-        return online;
+        return state instanceof OnlineState;
     }
 
     public void setOnline(boolean online) {
         this.online = online;
     }
 
-    public void addFriend(String friendId) {
-        friendIds.add(friendId);
-    }
-
-    public void removeFriend(String friendId) {
-        friendIds.remove(friendId);
-    }
+    public void addFriend(String friendId) { friendIds.add(friendId); }
+    public void removeFriend(String friendId) { friendIds.remove(friendId); }
 
     public boolean isFriend(String userId) {
         return friendIds.contains(userId);
     }
 
     public void addDirectMessage(String conversationId, Message message) {
-        directMessages
-            .computeIfAbsent(conversationId, k -> new ArrayList<>())
-            .add(message);
+        directMessages.computeIfAbsent(conversationId, k -> new ArrayList<>()).add(message);
     }
 
     public List<Message> getDirectMessages(String conversationId) {
@@ -95,25 +78,22 @@ public class User {
         return new HashSet<>(blockedUserIds);
     }
 
+    public void setState(UserState newState) {
+        this.state = newState;
+    }
+
+    public String getStatus() {
+        return state.getStateName();
+    }
 
     @Override
     public String toString() {
-        return (
-            "User{" +
-            "userId='" +
-            userId +
-            '\'' +
-            ", username='" +
-            username +
-            '\'' +
-            ", email='" +
-            email +
-            '\'' +
-            ", online=" +
-            online +
-            ", friendsCount=" +
-            friendIds.size() +
-            '}'
-        );
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", online=" + isOnline() +
+                ", friendsCount=" + friendIds.size() +
+                '}';
     }
 }
